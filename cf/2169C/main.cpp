64a1c81ca85794d997470a5f074947f1064e57be
Mihai <mihaimarandici@icloud.com>
2026-03-30 22:02:32 +0300

    2169C - attempt

--- cf/2169C/main.cpp
+++ cf/2169C/main.cpp
@@ -1,53 +1,45 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <algorithm>
 using namespace std;
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
+    
     int t;
     cin >> t;
-    while (t--) {
+
+    while(t--) {
         int n;
         cin >> n;
-        vector<long long> a(n + 1);
+        int a[n];
+        for (int i = 0; i < n; i++) cin >> a[i];
 
-        for (int i = 1; i <= n; i++) cin >> a[i];
+        long long pf[n]; pf[0] = a[0];
+        for (int i = 1; i < n; i++) pf[i] = pf[i - 1] + a[i];
 
-        long long total = 0;
-        for (int i = 1; i <= n; i++) total += a[i];
-
-        long long best = 0;
-        long long cur_sum = 0;
-        int l = 1;
-
-        for (int r = 1; r <= n; r++) {
-            cur_sum += a[r];
-
-            // Try shrinking from left
+        long long ans = pf[n - 1];
+        for (int l = 0; l < n; l++) {
+            int r = n - 1;
             while (l <= r) {
-                long long gain_now =
-                    (long long)(r - l + 1) * (l + r) - cur_sum;
+                long long displacedSum = pf[r] - pf[l] + a[l];
+                long long replacement = (l + r + 2) * (r - l + 1);
 
-                long long gain_if_shrink =
-                    (long long)(r - (l + 1) + 1) * ((l + 1) + r)
-                    - (cur_sum - a[l]);
+                long long gainNow = (l + r + 2) * (r - l + 1) - (pf[r] - pf[l] + a[l]);
+                long long gainIfShrink = (l + r + 1) * (r - l) - (pf[r] - pf[l] + a[l]) - a[r];
 
-                if (gain_if_shrink >= gain_now) {
-                    cur_sum -= a[l];
-                    l++;
+                if (gainNow >= gainIfShrink) {
+                    ans = max(ans, pf[n - 1] - displacedSum + replacement);
                 } else {
                     break;
                 }
+                r--;
             }
-
-            long long gain =
-                (long long)(r - l + 1) * (l + r) - cur_sum;
-
-            best = max(best, gain);
         }
-
-        cout << total + best << "\n";
+        cout << ans << endl;
     }
+    
+    return 0;
 }
